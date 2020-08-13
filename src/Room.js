7287e5c6afc80cfb936a19d2c8a91aaa16fc5b22
Ali Razzaq <alirazzaquet@gmail.com>
2020-08-13 14:10:44 +0500

    Update Room.js

--- src/Room.js
+++ src/Room.js
@@ -11,28 +11,31 @@ function Room() {
 
     return (
     <div className = {"room "+(isLit? "lit":"dark")}>
-        The Room is {isLit? "lit":"dark"}
+        <br />
+        <br />     
+        The Room is {isLit? "lit":"dark"} <br /> Press button to change that :)
+        <br />
         <br />
         <button onClick={() => setLit(!isLit)} > ON/OFF </button>
         <br />
         <br />
+    
         Age: {age}
         <br />
         <br />
-        <button onClick={() => setAge(age+1)} > Increase Age </button>
-        <br />
-        <button onClick={() => setAge(age-1)} > Decrease Age </button>
-        <br />
+        <button onClick={() => setAge(age-1)} > - </button>
         <button onClick={() => setAge(0)} > Set Zero </button>
+        <button onClick={() => setAge(50)} > Set Fifty </button>
+        <button onClick={() => setAge(age+1)} > + </button>
         <br />
-        <button onClick={() => setAge(50)} > Set Hundered </button>
         <br />
-        <br />
-        Current temeprature: {temp}
-        <br />
-        <button onClick={() => setTemp(temp+1)} > + </button>  
+
+        Current temeprature: {temp} *C
+        <br /> <br />
+        <button onClick={() => setTemp(temp-1)} > - </button>  
         <button onClick={() => setTemp(22)} > Default </button>
-        <button onClick={() => setTemp(temp-1)} > - </button>
+        <button onClick={() => setTemp(temp+1)} > + </button>
+
     </div>
   );
 }
